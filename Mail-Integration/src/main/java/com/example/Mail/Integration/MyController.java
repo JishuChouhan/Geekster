ecffbbf81ba0efc93939585fb1205a64d3f51f52
Jishu Chouhan <honeyjchouhan@gmail.com>
2023-10-27 17:53:06 +0530

    Mail

--- Mail-Integration/src/main/java/com/example/Mail/Integration/MyController.java
+++ Mail-Integration/src/main/java/com/example/Mail/Integration/MyController.java
@@ -1,26 +1,30 @@
 package com.example.Mail.Integration;
 
+import jakarta.annotation.Nullable;
 import org.eclipse.angus.mail.util.logging.MailHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Properties;
 
 @RestController
 public class MyController {
     @Autowired
     private MyObject myObject;
-    public static Sub email;
+    public static SimpleMailMessage email;
     @GetMapping("objects")
     public String getObjects() {
         // Use the created objects
         String myObjectAsString = myObject.toString();
 
         // Send the objects as strings via email
-        email.setsub(myObjectAsString);
+        email.setSubject(myObjectAsString);
         MailHandler MailHandler = new MailHandler();
 
-        MailHandler.sendmail();
+        MailHandler.setMailProperties(new Properties());
         return myObjectAsString;
     }
 }
